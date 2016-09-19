32d9ff5fb0495a9d29281d23cf634d03caafd913
Geoffroy Couprie <geo.couprie@gmail.com>
2016-09-19 17:53:02 +0200

    Check argument count

--- main.cpp
+++ main.cpp
@@ -1,3 +1,19 @@
-int main() {
-  return 128;
+#include <iostream>
+using namespace std;
+
+void usage() {
+  cout << "Usage:" << endl
+       << "\t./todo add \"do stuff\"" << endl
+       << "\t./todo remove 0" << endl
+       << "\t./todo list" << endl;
+}
+
+int main(int argc, char*argv[]) {
+  if(argc < 2) {
+    usage();
+    return 1;
+  }
+  cout << "nb d'arguments: " << argc << endl;
+  cout << "nom du programme: " << argv[0] << endl;
+  return 0;
 }
