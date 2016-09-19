b98b74e2685626bf977e57037b5115cab6f53ef9
Geoffroy Couprie <geo.couprie@gmail.com>
2016-09-19 18:20:23 +0200

    Use program name in usage

--- main.cpp
+++ main.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 using namespace std;
 
-void usage() {
+void usage(string name) {
   cout << "Usage:" << endl
-       << "\t./todo add \"do stuff\"" << endl
-       << "\t./todo remove 0" << endl
-       << "\t./todo list" << endl;
+       << "\t" << name << " add \"do stuff\"" << endl
+       << "\t" << name << " remove 0" << endl
+       << "\t" << name << " list" << endl;
 }
 
 int main(int argc, char*argv[]) {
+  string name = argv[0];
   if(argc < 2) {
-    usage();
+    usage(name);
     return 1;
   }
   cout << "nb d'arguments: " << argc << endl;
