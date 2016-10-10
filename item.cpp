820c2ed960a42165ab561b73682384642a472dfa
Geoffroy Couprie <geo.couprie@gmail.com>
2016-10-10 11:38:17 +0200

    add the finished field

--- item.cpp
+++ item.cpp
@@ -4,25 +4,39 @@ using namespace std;
 #include "item.h"
 
 Item::Item() {
-  title = "";
-  text  = "";
+  title    = "";
+  text     = "";
+  finished = false;
 }
 
 Item::Item(string _title, string _text) {
-  title = _title;
-  text = _text;
+  title    = _title;
+  text     = _text;
+  finished = false;
+}
+
+Item::Item(string _title, string _text, bool _finished) {
+  title    = _title;
+  text     = _text;
+  finished = _finished;
 }
 
 void Item::Print() {
   if(title != "" || text != "") {
-    cout << " title: " << title << endl
+    string crossed = " ";
+    if(finished) {
+      crossed = "X";
+    }
+
+    cout << "[" << crossed << "] title: " << title << endl
          << "text: " << text << endl;
   }
 }
 
-void Item::Change(string _title, string _text) {
-  title = _title;
-  text = _text;
+void Item::Change(string _title, string _text, bool _finished) {
+  title    = _title;
+  text     = _text;
+  finished = _finished;
 }
 
 string Item::Title() {
@@ -31,4 +45,12 @@ string Item::Title() {
 
 string Item::Text() {
   return text;
+}
+
+bool Item::Finished() {
+  return finished;
+}
+
+void Item::Toggle() {
+  finished = !finished;
 }
--- item.h
+++ item.h
@@ -7,14 +7,18 @@ using namespace std;
 class Item {
   string title;
   string text;
+  bool   finished;
 
   public:
     Item();
     Item(string,string);
+    Item(string,string,bool);
     void Print();
-    void Change(string title, string text);
+    void Change(string title, string text, bool finished);
     string Title();
     string Text();
+    bool   Finished();
+    void   Toggle();
 };
 
 #endif
