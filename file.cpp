ca91843e3038f38d8dae2e03ee45c5528572a146
Geoffroy Couprie <geo.couprie@gmail.com>
2016-10-10 11:58:18 +0200

    handle new format with version

--- file.cpp
+++ file.cpp
@@ -12,8 +12,10 @@ void write_items(vector<Item> items) {
   ofstream wfile;
   wfile.open ("data.db");
   if (wfile.is_open()) {
+    wfile << '\0' << '1';
     for(Item it: items) {
-      wfile << it.Title() << '\x1F' << it.Text() << '\x1E';
+      wfile << it.Title() << '\x1F' << it.Text()
+            << '\x1F' << it.Finished() << '\x1E';
     }
     wfile.close();
   } else {
@@ -26,7 +28,14 @@ string read_file();
 
 void read_items(vector<Item>& items) {
   string content = read_file();
-  string::size_type prev_pos = 0, pos = 0;
+  string header  = content.substr(0, 2);
+  if(header[0] != '\0' && header[1] != '1') {
+    cout << "unrecognized format: '" << header
+         << "' should be '\0" << "1'" << endl;
+    return;
+  }
+  string::size_type prev_pos = 2, pos = 2;
+
   char unit_separator   = '\x1F';
   char record_separator = '\x1E';
 
@@ -34,11 +43,18 @@ void read_items(vector<Item>& items) {
     string title( content.substr(prev_pos, pos-prev_pos) );
 
     prev_pos = ++pos;
-    if((pos = content.find(record_separator, pos)) != string::npos) {
-      string text( content.substr(prev_pos, pos-prev_pos) );
+    if((pos = content.find(unit_separator, pos)) != string::npos) {
+      string text = ( content.substr(prev_pos, pos-prev_pos) );
 
-      items.push_back(Item(title, text));
       prev_pos = ++pos;
+      if((pos = content.find(record_separator, pos)) != string::npos) {
+        string finished( content.substr(prev_pos, pos-prev_pos) );
+
+        items.push_back(Item(title, text));
+        prev_pos = ++pos;
+    } else {
+      break;
+    }
     } else {
       break;
     }
